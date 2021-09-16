d96a64ca9359b4f3437c7aab6182a9966f16d78d
SONG22 <ansongi527@gmail.com>
2021-09-16 11:46:46 +0900

    10-1 controller - flux

--- webflux-demo/src/main/java/com/songi/webfluxdemo/WebfluxDemoApplication.java
+++ webflux-demo/src/main/java/com/songi/webfluxdemo/WebfluxDemoApplication.java
@@ -1,13 +1,55 @@
 package com.songi.webfluxdemo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
+@Slf4j
+@RestController
 public class WebfluxDemoApplication {
+
+    @GetMapping("/event/{id}")
+    Mono<Event> hello(@PathVariable long id) {
+        return Mono.just(new Event(id, "event" + id));
+    }
+
+    @GetMapping("/events/{id}")
+    Mono<List<Event>> monoList(@PathVariable long id) {
+        List<Event> list = Arrays.asList(new Event(1L, "event1"), new Event(2L, "event2"));
+        return Mono.just(list);
+    }
+
+    @GetMapping("/events/flux")
+    Flux<Event> fluxEvents(){
+        return Flux.just(new Event(1L, "event1"), new Event(2L, "event2"));
+    }
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Event> fluxEvents2(){
+        List<Event> list = Arrays.asList(new Event(1L, "event1"), new Event(2L, "event2"));
+        return Flux.fromIterable(list); // http stream 을 지원하는 것은 Flux 를 쓰면 편리하다.
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebfluxDemoApplication.class, args);
     }
 
+    @Data
+    @AllArgsConstructor
+    public static class Event {
+        long id;
+        String value;
+    }
 }
